f3528517b2b6f06da2bf2dc91f0ae1fd95c726f1
Andrew Wang <wangy7531@gmail.com>
2017-10-27 01:21:50 +0000

    Added Twitter Bootstrap via SASS gem. Also implemented global navigation bar.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,19 @@
-#The pages controller contains all of the code for any pages inside /pages
+# The pages controller contains all of the code for any pages inside /pages
 class PagesController < ApplicationController
   
-  #back-end code for pages/index
+  # back-end code for pages/index
   def index
   end
   
-  #back-end code for pages/home
+  # back-end code for pages/home
   def home
   end
 
-  #back-end code for pages/profile
+  # back-end code for pages/profile
   def profile
   end
   
-  #back-end code for pages/explore
+  # back-end code for pages/explore
   def explore
   end
 end
